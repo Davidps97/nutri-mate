2c7651869f0c8bb3353036475496ed651788f5b4
David Pérez Santana <davidperezsantana1997@gmail.com>
2024-05-21 19:30:40 +0200

    front call try

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { getFoods } from "@/services/FoodServices";
+
 export default function Home() {
+  const [foodData, setFoodData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getFoods("rice 100gr");
+      setFoodData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div>aaaaa</div>
+      {foodData && (
+        <div>
+          <h2>Información nutricional:</h2>
+          <p>Nombre: {foodData.ingredients[0].text}</p>
+          <p>Calorías: {foodData.calories}</p>
+          <p>Peso total: {foodData.totalWeight}</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ frontend/src/services/FoodServices.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const app_id = "367af0ac";
+const app_key = "3fdff35c2ae782d1be34fbfb37fe4176";
+
+export const getFoods = async (ingredient) => {
+  try {
+    const response = await axios.get(
+      "https://api.edamam.com/api/nutrition-data",
+      {
+        params: {
+          app_id: app_id,
+          app_key: app_key,
+          "nutrition-type": "cooking",
+          ingr: ingredient,
+        },
+        headers: {
+          accept: "application/json",
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
